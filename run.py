f01d1f3c4dcad078ca48fe93de2b16f79d83d5c9
Ubuntu <ubuntu@ip-172-31-93-228.ec2.internal>
2019-07-19 12:10:04 +0000

    Added the ability to add to and read from a list of messages and display to the user

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
 import os
-from flask import Flask
+
+# import redirect, to be able to redirect from one route to another
+from flask import Flask, redirect
 
 app = Flask(__name__)
+
+# Create an empty list for messages
+messages = []
+
+""" Create a function to add user name and messages to the messages list """
+
+def add_messages(username, message):
+    """ Use the append method to add username and message to messages """
+    messages.append("{}: {}".format(username, message))
+    
+
+def get_all_messages():
+    """Get all the messages and separate using a 'br' """
+    return "<br>".join(messages)
 
 @app.route('/')
 def index():
@@ -14,11 +30,16 @@ def index():
     
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """ Display chat messages """
+    return "<h1>Welcome, {}</h1>{}".format(username, get_all_messages())
     
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """ Create a new message and redirect to the chat page """
+    add_messages(username, message)
+    return redirect("/" + username)
+    
+#    return "{0}: {1}".format(username, message)
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
 
